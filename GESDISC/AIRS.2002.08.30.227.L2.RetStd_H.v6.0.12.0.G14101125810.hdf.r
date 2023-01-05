753dbb75452b6316987ea54939d6a9e03fdbc08c
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2023-01-04 20:33:27 -0600

    test: print values

--- GESDISC/AIRS.2002.08.30.227.L2.RetStd_H.v6.0.12.0.G14101125810.hdf.r
+++ GESDISC/AIRS.2002.08.30.227.L2.RetStd_H.v6.0.12.0.G14101125810.hdf.r
@@ -32,8 +32,9 @@ download.file("https://gamma.hdfgroup.org/ftp/pub/outgoing/NASAHDF/AIRS.2002.08.
 # Open file.
 fname <- 'AIRS.2002.08.30.227.L2.RetStd_H.v6.0.12.0.G14101125810.hdf'
 nc <- open.nc(fname)
-print.nc(nc)
+# print.nc(nc)
 nc_data <- read.nc(nc)
+print(nc_data)
 z_all <- nc_data$topog
 zv <- as.vector(as.single(z_all))
 zz <- file("tmpbin", "wb")
@@ -46,29 +47,32 @@ dim(zs) <- dim(z_all)
 b <- zs[,]
 b[b == -9999.0] <- NA
 lat <- nc_data$Latitude
+print(lat)
 lon <- nc_data$Longitude
+print(lon)
 remap.tbl <- data.frame(coordinates(lon),
                         lon=as.vector(lon),lat=as.vector(lat))
+print(b)
 df <- data.frame(lon=as.vector(lon), lat=as.vector(lat), rad=as.vector(b))
 png(paste(fname, ".r.png", sep=""), width=640, height=480)
-mapWorld <- borders("world") 
+# mapWorld <- borders("world") 
 wd <- map_data("world")
-ggplot() +
-mapWorld +
-scale_colour_gradient2(name = "topog", low="blue", mid="green", high="red", na.value="white", midpoint=60) +
-geom_point(data = df, aes(x = lon, y = lat, colour=rad), na.rm=TRUE) +
-ggtitle(fname) +
-ylab("Latitude") +
-xlab("Longitude") +
-theme_linedraw() +
-theme(plot.title = element_text(size = 15, face = "bold"),
-legend.title = element_text(size = 15),
-axis.text = element_text(size = 15),
-axis.text.x = element_blank(),
-axis.title.x = element_blank(),
-axis.title.y = element_text(size = 20, vjust = 0.2),
-legend.text = element_text(size = 10)) +
-coord_map("stereographic", orientation=c(90, 0,0), ylim=c(90, 60))
+ggplot() # +
+# mapWorld +
+# scale_colour_gradient2(name = "topog", low="blue", mid="green", high="red", na.value="white", midpoint=60) +
+# geom_point(data = df, aes(x = lon, y = lat, colour=rad), na.rm=TRUE) +
+# ggtitle(fname) +
+# ylab("Latitude") +
+# xlab("Longitude") +
+# theme_linedraw() +
+# theme(plot.title = element_text(size = 15, face = "bold"),
+# legend.title = element_text(size = 15),
+# axis.text = element_text(size = 15),
+# axis.text.x = element_blank(),
+# axis.title.x = element_blank(),
+# axis.title.y = element_text(size = 20, vjust = 0.2),
+# legend.text = element_text(size = 10)) +
+# coord_map("stereographic", orientation=c(90, 0,0), ylim=c(90, 60))
 dev.off()
 close.nc(nc)
 print(getwd())
