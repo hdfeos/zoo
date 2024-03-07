94fc7db7c6c14d87bd41eb1210d4e1a76f67995d
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2024-03-07 11:05:39 -0600

    feat: update ATL16 to v5

---
+++ ICESat2/ATL16_20210101010136_01231001_005_01_h5.m
@@ -0,0 +1,113 @@
+%
+%  This example code illustrates how to access and visualize an
+% NSIDC ICESat-2 ATL16 L3B HDF5 file in MATLAB. 
+%
+%  If you have any questions, suggestions, comments on this example, please 
+% use the HDF-EOS Forum (http://hdfeos.org/forums). 
+% 
+%  If you would like to see an  example of any other NASA HDF/HDF-EOS data 
+% product that is not listed in the HDF-EOS Comprehensive Examples page 
+% (http://hdfeos.org/zoo), feel free to contact us at eoshelp@hdfgroup.org 
+% or post it at the HDF-EOS Forum (http://hdfeos.org/forums).
+% Usage:save this script and run (without .m at the end)
+%
+%
+% $matlab -nosplash -nodesktop -r ATL16_20210101010136_01231001_005_01_h5
+%
+% Tested under: MATLAB R2023b
+% Last updated: 2024-03-07
+
+clear
+
+% Open the HDF5 File.
+FILE_NAME = 'ATL16_20210101010136_01231001_005_01.h5';
+file_id = H5F.open (FILE_NAME, 'H5F_ACC_RDONLY', 'H5P_DEFAULT');
+
+% Open the dataset.
+DATAFIELD_NAME = 'global_asr';
+data_id = H5D.open (file_id, DATAFIELD_NAME);
+
+Lat_NAME='global_grid_lat';
+lat_id=H5D.open(file_id, Lat_NAME);
+
+Lon_NAME='global_grid_lon';
+lon_id=H5D.open(file_id, Lon_NAME);
+
+% Read the dataset.
+data=H5D.read(data_id,'H5T_NATIVE_DOUBLE', 'H5S_ALL', 'H5S_ALL', 'H5P_DEFAULT');
+
+lat=H5D.read(lat_id,'H5T_NATIVE_DOUBLE', 'H5S_ALL', 'H5S_ALL', 'H5P_DEFAULT');
+
+lon=H5D.read(lon_id,'H5T_NATIVE_DOUBLE', 'H5S_ALL', 'H5S_ALL', 'H5P_DEFAULT');
+
+
+% Read the fill value.
+ATTRIBUTE = '_FillValue';
+attr_id = H5A.open_name (data_id, ATTRIBUTE);
+fillvalue=H5A.read (attr_id, 'H5T_NATIVE_DOUBLE');
+
+% Read the units.
+ATTRIBUTE = 'units';
+attr_id = H5A.open_name (data_id, ATTRIBUTE);
+units = H5A.read(attr_id, 'H5ML_DEFAULT');
+
+% Read the valid_max.
+ATTRIBUTE = 'valid_max';
+attr_id = H5A.open_name (data_id, ATTRIBUTE);
+valid_max = H5A.read(attr_id, 'H5ML_DEFAULT');
+
+% Read the valid_min.
+ATTRIBUTE = 'valid_min';
+attr_id = H5A.open_name (data_id, ATTRIBUTE);
+valid_min = H5A.read(attr_id, 'H5ML_DEFAULT');
+
+% Read title attribute.
+ATTRIBUTE = 'long_name';
+attr_id = H5A.open_name (data_id, ATTRIBUTE);
+long_name=H5A.read (attr_id, 'H5ML_DEFAULT');
+
+% Close and release resources.
+H5A.close (attr_id)
+H5D.close (data_id);
+H5F.close (file_id);
+
+% Replace the fill value with NaN.
+data(data==fillvalue) = NaN;
+
+f=figure('Name', FILE_NAME, 'Renderer', 'zbuffer', ...
+         'Position', [0,0,800,600], ...
+         'visible', 'off');
+
+% Create the plot.
+axesm('MapProjection','eqdcylin',...
+      'Frame','on','Grid','on', ...
+      'MeridianLabel','on','ParallelLabel','on','MLabelParallel', ...
+      'south')
+
+% Plot the data.
+cm = colormap('Jet');
+surfm(lat, lon, data.');
+coast = load('coastlines.mat');
+plotm(coast.coastlat, coast.coastlon,'k');
+tightmap;
+
+h = colorbar();
+
+% An HDF5 string attribute is an array of characters.
+% Without the following conversion, the characters in unit will appear 
+% in a vertical direction.
+units1 = sprintf('%s', char(units));
+
+% lunits is pretty long so use a small font.
+set (get(h, 'title'), 'string', units1, 'FontSize', 8, ...
+                   'Interpreter', 'None', ...
+                   'FontWeight','bold');
+  
+name = sprintf('%s', long_name);
+
+% long_name is also long so we use a small font.
+title({FILE_NAME; name}, ... 
+      'Interpreter', 'None', 'FontSize', 10,'FontWeight','bold');
+
+saveas(f, [FILE_NAME '.m.png']);
+exit;
