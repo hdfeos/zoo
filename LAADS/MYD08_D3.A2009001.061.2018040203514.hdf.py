0415abf078fbac8f81587127b478728721dc677c
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2024-05-31 15:26:01 -0500

    feat: update MYD08_D3 to v6.1
    close #47

---
+++ LAADS/MYD08_D3.A2009001.061.2018040203514.hdf.py
@@ -0,0 +1,100 @@
+"""
+This example code illustrates how to access and visualize a LAADS MYD08_D3 v6.1
+HDF-EOS2 Grid file in Python.
+
+If you have any questions, suggestions, or comments on this example, please use
+the HDF-EOS Forum (http://hdfeos.org/forums).  If you would like to see an
+example of any other NASA HDF/HDF-EOS data product that is not listed in the
+HDF-EOS Comprehensive Examples page (http://hdfeos.org/zoo), feel free to
+contact us at eoshelp@hdfgroup.org or post it at the HDF-EOS Forum
+(http://hdfeos.org/forums).
+
+Usage:  save this script and run
+
+    $python MYD08_D3.A2009001.061.2018040203514.hdf.py
+
+The HDF-EOS2 file must be in your current working directory.
+
+Tested under: Python 3.9.13 :: Miniconda (x86_64)
+Last updated: 2024-05-31
+"""
+
+import os
+
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+import numpy as np
+from cartopy.mpl.gridliner import LATITUDE_FORMATTER, LONGITUDE_FORMATTER
+from pyhdf.SD import SD, SDC
+
+FILE_NAME = "MYD08_D3.A2009001.061.2018040203514.hdf"
+DATAFIELD_NAME = "Cloud_Fraction_Mean"
+
+hdf = SD(FILE_NAME, SDC.READ)
+
+# Read dataset.
+data_raw = hdf.select(DATAFIELD_NAME)
+data = data_raw[:, :].astype(np.double)
+
+# Read lat/lon.
+xdim = hdf.select("XDim")
+lon = xdim[:].astype(np.double)
+
+ydim = hdf.select("YDim")
+lat = ydim[:].astype(np.double)
+
+# Retrieve attributes.
+attrs = data_raw.attributes(full=1)
+lna = attrs["long_name"]
+long_name = lna[0]
+aoa = attrs["add_offset"]
+add_offset = aoa[0]
+fva = attrs["_FillValue"]
+_FillValue = fva[0]
+sfa = attrs["scale_factor"]
+scale_factor = sfa[0]
+ua = attrs["units"]
+units = ua[0]
+
+# Retrieve dimension name.
+dim = data_raw.dim(0)
+dimname = dim.info()[0]
+
+data[data == _FillValue] = np.nan
+data = scale_factor * (data - add_offset)
+datam = np.ma.masked_array(data, np.isnan(data))
+
+# Use Geographic projection.
+ax = plt.axes(projection=ccrs.PlateCarree())
+
+# Plot on map.
+p = plt.pcolormesh(lon, lat, datam, transform=ccrs.PlateCarree())
+
+# Put coast lines.
+ax.coastlines()
+
+# Put grids.
+gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True)
+
+# Put grid labels only at left and bottom.
+# gl.xlabels_top = False
+gl.top_labels = False
+# gl.ylabels_right = False
+gl.right_labels = False
+
+# Put degree N/E label.
+gl.xformatter = LONGITUDE_FORMATTER
+gl.yformatter = LATITUDE_FORMATTER
+
+# Adjust colorbar size and location using fraction and pad.
+cb = plt.colorbar(p, fraction=0.022, pad=0.01)
+cb.set_label(units, fontsize=8)
+
+# Put title.
+basename = os.path.basename(FILE_NAME)
+plt.title("{0}\n{1}".format(basename, long_name), fontsize=8)
+fig = plt.gcf()
+
+# Save image.
+pngfile = "{0}.py.png".format(basename)
+fig.savefig(pngfile)
---
+++ LAADS/MYD08_D3_A2009001_061_2018040203514_hdf.m
@@ -0,0 +1,137 @@
+%
+% This example code illustrates how to access and visualize LAADS
+% MYD08_D3 v6.1 HDF-EOS2 Grid file in MATLAB. 
+%
+%  If you have any questions, suggestions, comments on this example, please 
+% use the HDF-EOS Forum (http://hdfeos.org/forums). 
+%
+%  If you would like to see an  example of any other NASA HDF/HDF-EOS data 
+% product that is not listed in the HDF-EOS Comprehensive Examples page 
+% (http://hdfeos.org/zoo), feel free to contact us at eoshelp@hdfgroup.org or 
+% post it at the HDF-EOS Forum (http://hdfeos.org/forums).
+%
+% Usage: save this script and run (without .m at the end)
+% 
+% $matlab -nosplash -nodesktop -r MYD08_D3_A2009001_061_2018040203514_hdf
+%
+% Tested under: MATLAB R2023b
+% Last updated: 2024-05-31
+
+
+clear
+
+import matlab.io.hdfeos.*
+import matlab.io.hdf4.*
+
+% Read data field
+FILE_NAME='MYD08_D3.A2009001.061.2018040203514.hdf';
+GRID_NAME='mod08';
+
+% Opening the HDF-EOS2 Grid File
+file_id = gd.open(FILE_NAME, 'rdonly');
+
+% Open Grid
+grid_id = gd.attach(file_id, GRID_NAME);
+
+% Define the Data Field
+DATAFIELD_NAME='Cloud_Fraction_Mean';
+
+
+% Read the dataset.
+data = gd.readField(grid_id, DATAFIELD_NAME, [], [], []);
+
+% Read lat and lon dataset.
+lon = gd.readField(grid_id, 'XDim', [], [], []);
+lat = gd.readField(grid_id, 'YDim', [], [], []);
+
+% Detach Grid object.
+gd.detach(grid_id);
+gd.close(file_id);
+
+% Transpose the data to match the map projection.
+data=data';
+
+% Convert the data to double type for plot.
+data=double(data);
+lon=double(lon);
+lat=double(lat);
+
+% Read attributes from the data field.
+SD_id = sd.start(FILE_NAME, 'rdonly');
+sds_index = sd.nameToIndex(SD_id, DATAFIELD_NAME);
+sds_id = sd.select(SD_id, sds_index);
+
+% Read _FillValue from data field.
+fillvalue_index = sd.findAttr(sds_id, '_FillValue');
+fillvalue = sd.readAttr(sds_id, fillvalue_index);
+
+% Get the long name from data field.
+long_name_index = sd.findAttr(sds_id, 'long_name');
+long_name = sd.readAttr(sds_id, long_name_index);
+
+% Read units from the data field.
+units_index = sd.findAttr(sds_id, 'units');
+units = sd.readAttr(sds_id, units_index);
+
+% Read scale_factor from the data field.
+scale_index = sd.findAttr(sds_id, 'scale_factor');
+scale = sd.readAttr(sds_id, scale_index);
+scale = double(scale);
+
+% Read add_offset from the data field.
+offset_index = sd.findAttr(sds_id, 'add_offset');
+offset = sd.readAttr(sds_id, offset_index);
+offset = double(offset);
+
+% Read valid_range from the data field.
+range_index = sd.findAttr(sds_id, 'valid_range');
+range = sd.readAttr(sds_id, range_index);
+
+% Terminate access to the corresponding data set.
+sd.endAccess(sds_id);
+
+% Close the file.
+sd.close(SD_id);
+
+% Replace the filled value with NaN.
+data(data==fillvalue) = NaN;
+data(data > double(range(2))) = NaN;
+data(data < double(range(1))) = NaN;
+
+% Multiply scale and add offset, the equation is scale *(data-offset).
+data = scale*(data-offset);
+
+% create the graphics figure -- 'visible'->'off' = off-screen rendering
+f=figure('Name', FILE_NAME, ...
+	 'Renderer', 'zbuffer', ...
+	 'Position', [0,0,800,600], ...
+	 'visible', 'off');
+axesm('MapProjection','eqdcylin', ...
+      'Frame','on','Grid','on', ...
+      'FontSize', 5, ...
+      'MeridianLabel','on','ParallelLabel','on', ...
+      'MLabelParallel','south');      
+
+
+% surfm() is faster than controufm.
+surfm(lat, lon, data);
+
+% Load the coastlines data file.
+coast = load('coastlines.mat');
+
+% Plot coastlines in color black ('k').
+plotm(coast.coastlat, coast.coastlon, 'k')
+tightmap;
+
+% Put colormap.
+colormap('Jet');
+h=colorbar();
+set (get(h, 'title'), 'string', units);
+
+% Set the title using long_name.
+title({FILE_NAME; long_name}, ...
+      'interpreter', 'none', 'FontSize',10,'FontWeight','bold');
+saveas(f, [FILE_NAME '.m.png']);
+exit;
+
+
