c27f5d40a77d34ef2924067723532460d263eae8
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2024-07-31 13:50:01 -0500

    feat(LAADS): update MYD021KM to v6.1

---
+++ LAADS/MYD021KM.A2002226.0000.061.2017363061238.hdf.py
@@ -0,0 +1,119 @@
+"""
+This example code illustrates how to access and visualize a LAADS MYD021KM v6.1
+HDF-EOS2 Swath file in Python.
+
+If you have any questions, suggestions, or comments on this example, please use
+the HDF-EOS Forum (http://hdfeos.org/forums).  If you would like to see an
+example of any other NASA HDF/HDF-EOS data product that is not listed in the
+HDF-EOS Comprehensive Examples page (http://hdfeos.org/zoo), feel free to
+contact us at eoshelp@hdfgroup.org or post it at the HDF-EOS Forum
+(http://hdfeos.org/forums).
+
+Usage:  save this script and run
+
+  $python MYD021KM.A2002226.0000.061.2017363061238.hdf.py
+
+The HDF-EOS2 files must be in your current working directory.
+
+Tested under: Python 3.9.13 :: Miniconda
+Last updated: 2024-07-31
+"""
+
+import os
+
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+import numpy as np
+from pyhdf.SD import SD, SDC
+
+FILE_NAME = "MYD021KM.A2002226.0000.061.2017363061238.hdf"
+GEO_FILE_NAME = "MYD03.A2002226.0000.061.2017362193857.hdf"
+DATAFIELD_NAME = "EV_1KM_Emissive"
+
+# Open file.
+hdf = SD(FILE_NAME, SDC.READ)
+
+# Read dataset.
+data_raw = hdf.select(DATAFIELD_NAME)
+
+# Subset data.
+data = data_raw[0, :, :].astype(np.double)
+
+# Open MYD03 geolocation file.
+hdf_geo = SD(GEO_FILE_NAME, SDC.READ)
+
+# Read geolocation dataset from MYD03 product.
+lat = hdf_geo.select("Latitude")
+latitude = lat[:, :]
+lon = hdf_geo.select("Longitude")
+longitude = lon[:, :]
+
+# Retrieve attributes.
+attrs = data_raw.attributes(full=1)
+lna = attrs["long_name"]
+long_name = lna[0]
+aoa = attrs["radiance_offsets"]
+add_offset = aoa[0][0]
+fva = attrs["_FillValue"]
+_FillValue = fva[0]
+sfa = attrs["radiance_scales"]
+scale_factor = sfa[0][0]
+vra = attrs["valid_range"]
+valid_min = vra[0][0]
+valid_max = vra[0][1]
+ua = attrs["radiance_units"]
+units = ua[0]
+
+# Retrieve dimension name.
+dim = data_raw.dim(0)
+dimname = dim.info()[0]
+
+invalid = np.logical_or(data > valid_max, data < valid_min)
+invalid = np.logical_or(invalid, data == _FillValue)
+data[invalid] = np.nan
+data = (data - add_offset) * scale_factor
+data = np.ma.masked_array(data, np.isnan(data))
+
+# Find middle location.
+lat_m = np.nanmean(latitude)
+lon_m = np.nanmean(longitude)
+
+# Let's use ortho projection.
+orth = ccrs.Orthographic(
+    central_longitude=lon_m, central_latitude=lat_m, globe=None
+)
+ax = plt.axes(projection=orth)
+
+# Set global view. You can comment it out to get zoom-in view.
+ax.set_global()
+
+# If you want to get global view, you need to subset.
+p = plt.pcolormesh(
+    longitude[::5][::5],
+    latitude[::5][::5],
+    data[::5][::5],
+    transform=ccrs.PlateCarree(),
+)
+
+# The following works if global view is turned off.
+# p = plt.pcolormesh(longitude, latitude, data, transform=ccrs.PlateCarree())
+
+# Gridline with draw_labels=True doesn't work on Ortho projection.
+# ax.gridlines(draw_labels=True)
+ax.gridlines()
+ax.coastlines()
+cb = plt.colorbar(p)
+cb.set_label(units, fontsize=8)
+
+basename = os.path.basename(FILE_NAME)
+plt.title(
+    "{0}\n{1}\nat {2}=0".format(
+        basename, "Radiance derived from " + long_name, dimname
+    ),
+    fontsize=10,
+)
+fig = plt.gcf()
+
+# Save file.
+pngfile = "{0}.py.png".format(basename)
+fig.savefig(pngfile)
---
+++ LAADS/MYD021KM_A2002226_0000_061_2017363061238_hdf.m
@@ -0,0 +1,157 @@
+%
+% This example code illustrates how to access and visualize LAADS MYD021KM v6.1
+% HDF-EOS2 Swath file in MATLAB. 
+%
+%  If you have any questions, suggestions, comments on this example, please 
+% use the HDF-EOS Forum (http://hdfeos.org/forums). 
+%
+%  If you would like to see an  example of any other NASA HDF/HDF-EOS data 
+% product that is not listed in the HDF-EOS Comprehensive Examples page 
+% (http://hdfeos.org/zoo), feel free to contact us at eoshelp@hdfgroup.org or 
+% post it at the HDF-EOS Forum (http://hdfeos.org/forums).
+%
+% Usage: save this script and run (without .m at the end)
+% 
+%   $matlab -nosplash -nodesktop -r MYD021KM_A2002226_0000_061_2017363061238_hdf
+%
+% Tested under: MATLAB R2023b
+% Last updated: 2024-07-31
+
+import matlab.io.hdfeos.*
+import matlab.io.hdf4.*
+
+% Open file.
+FILE_NAME='MYD021KM.A2002226.0000.061.2017363061238.hdf';
+file_id = sw.open(FILE_NAME, 'rdonly');
+
+% Open swath.
+SWATH_NAME='MODIS_SWATH_Type_L1B';
+swath_id = sw.attach(file_id, SWATH_NAME);
+
+% Read the dataset.
+DATAFIELD_NAME='EV_1KM_Emissive';
+data_raw = sw.readField(swath_id, DATAFIELD_NAME, [], [], []);
+
+% Detach Swath object.
+sw.detach(swath_id);
+
+% Close file.
+sw.close(file_id);
+
+% Open geo-location file.
+GEO_FILE_NAME='MYD03.A2002226.0000.061.2017362193857.hdf';
+file_id = sw.open(GEO_FILE_NAME, 'rdonly');
+
+% Open swath.
+SWATH_NAME='MODIS_Swath_Type_GEO';
+swath_id = sw.attach(file_id, SWATH_NAME);
+
+% Read lat and lon dataset.
+lon = sw.readField(swath_id, 'Longitude', [], [], []);
+lat = sw.readField(swath_id, 'Latitude', [], [], []);
+
+% Detach Swath object.
+sw.detach(swath_id);
+
+% Close file.
+sw.close(file_id);
+
+% Read attributes from the data field.
+SD_id = sd.start(FILE_NAME, 'rdonly');
+sds_index = sd.nameToIndex(SD_id, DATAFIELD_NAME);
+sds_id = sd.select(SD_id, sds_index);
+
+% Read _FillValue from data field.
+fillvalue_index = sd.findAttr(sds_id, '_FillValue');
+fillvalue = sd.readAttr(sds_id, fillvalue_index);
+
+% Get the long name from data field.
+long_name_index = sd.findAttr(sds_id, 'long_name');
+long_name = sd.readAttr(sds_id, long_name_index);
+
+% Read units from the data field.
+units_index = sd.findAttr(sds_id, 'radiance_units');
+units = sd.readAttr(sds_id, units_index);
+
+% Read scale_factor from the data field.
+scale_index = sd.findAttr(sds_id, 'radiance_scales');
+scale = sd.readAttr(sds_id, scale_index);
+scale = double(scale(1));
+
+% Read add_offset from the data field.
+offset_index = sd.findAttr(sds_id, 'radiance_offsets');
+offset = sd.readAttr(sds_id, offset_index);
+offset = double(offset(1));
+
+% Read valid_range from the data field.
+range_index = sd.findAttr(sds_id, 'valid_range');
+range = sd.readAttr(sds_id, range_index);
+
+% Terminate access to the corresponding data set.
+sd.endAccess(sds_id);
+
+% Close the file.
+sd.close(SD_id);
+
+% Subset 3-D data and make 2D.
+lev=0;
+data=squeeze(data_raw(:,:,lev+1));
+
+% Convert the data to double type for plot.
+data=double(data);
+lon=double(lon);
+lat=double(lat);
+
+% Replace the filled value with NaN.
+data(data==fillvalue) = NaN;
+data(data > double(range(2))) = NaN;
+data(data < double(range(1))) = NaN;
+
+% Multiply scale and add offset, the equation is scale *(data-offset).
+data = scale*(data-offset);
+
+% Set the map parameters.
+lon_c = mean(mean(lon));
+lat_c = mean(mean(lat));
+latlim=ceil(max(max(lat))) - floor(min(min(lat)));
+
+% create the graphics figure -- 'visible'->'off' = off-screen rendering
+f=figure('Name', FILE_NAME, ...
+	 'Renderer', 'zbuffer', ...
+	 'Position', [0,0,800,600], ...
+	 'visible', 'off');
+
+
+% FlatLimit will give us a zoom-in effect in Ortho projection.
+%       'FLatLimit', [-Inf, latlim], ...
+axesm ('ortho', 'Frame', 'on', 'Grid', 'on', ...
+       'origin', [lat_c, lon_c]);
+mlabel('equator');
+plabel(0); 
+plabel('fontweight','bold');
+
+% Load the coastlines data file.
+coast = load('coastlines.mat');
+
+% Plot coastlines in color black ('k').
+plotm(coast.coastlat, coast.coastlon, 'k');
+
+% surfm() is faster than controufm.
+surfm(lat, lon, data);
+
+% Put colormap.
+colormap('Jet');
+h=colorbar();
+set (get(h, 'title'), 'string', units);
+
+% Set the title using long_name.
+field_info = hdfinfo(FILE_NAME, 'eos');
+dname = field_info.Swath.DataFields(3).Dims(1).Name;
+title({FILE_NAME; ...
+      ['Radiance derived from ',long_name ]; ...
+      ['at ', strrep(dname,'_','\_'),'=',int2str(lev)]}, ...
+      'FontSize',10,'FontWeight','bold');
+saveas(f, [FILE_NAME '.m.png']);
+exit;
+
+
